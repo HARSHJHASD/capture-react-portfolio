6a8809f8ee24bdcfed572a0d247b54cd0b6f770d
Jean Clayton Seraphin <clayton0687@gmail.com>
2020-11-24 13:42:58 -0500

    React with github.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Film Hello World! </h1>
+      <h2> Merry Christmas !</h2>
+      <h3> Not yet ! </h3>
+    </div>
+  );
+}
+
+export default App;
