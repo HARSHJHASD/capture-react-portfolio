20924ab054cad823a7aa1e343b457a58883741c8
Jean Clayton Seraphin <clayton0687@gmail.com>
2020-11-25 14:13:23 -0500

    Styling Services Section.

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import home1 from '../img/home1.png';
+import {StyledAbout, StyledDescription, StyledHide, StyledImage} from '../styles'
 //styles
 import styled from 'styled-components';
 
@@ -33,35 +34,4 @@ const AboutSection = () => {
     )
 }
 
-const StyledAbout = styled.div`
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: white;
-`;
-
-const StyledDescription = styled.div`
-    flex: 1;
-    padding-right: 5rem;
-    h2{
-        font-weight: lighter;
-    }
-
-`;
-
-const StyledImage = styled.div`
-    flex: 1;
-    overflow: hidden;
-    img{
-        width: 100%;
-        height: 80vh;
-        object-fit: cover;
-    }
-`;
-
-const StyledHide = styled.div`
-    overflow: hidden;
-`;
 export default AboutSection;
