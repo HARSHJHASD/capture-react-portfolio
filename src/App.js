6e997d5f858de90b45a8b525acf2f27d9d91107d
Jean Clayton Seraphin <clayton0687@gmail.com>
2020-11-24 21:44:39 -0500

    Added AboutSection.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
+//import pages
+import AboutUs from './pages/AboutUs'
 
 function App() {
   return (
     <div className="App">
-      <h1> Film Hello World! </h1>
-      <h2> Merry Christmas !</h2>
-      <h3> Not yet ! </h3>
+      <AboutUs />
     </div>
   );
 }
