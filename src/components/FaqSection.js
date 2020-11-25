cafff6310d45b56a8c909bb855bb9ac82989ca9d
Jean Clayton Seraphin <clayton0687@gmail.com>
2020-11-24 22:27:47 -0500

    Added FAQ Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,49 @@
+import React from 'react';
+//import pages
+import AboutUs from '../pages/AboutUs';
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How do i start?</h4>
+                <div className="answer">
+                    <p>
+                        Proin eget tortor risus. Vestibulum ac diam sit amet quam vehicula
+                        elementum sed sit amet dui. Sed porttitor lectus nibh.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Dayli Schedule</h4>
+                <div className="answer">
+                    <p>
+                        Proin eget tortor risus. Vestibulum ac diam sit amet quam vehicula
+                        elementum sed sit amet dui. Sed porttitor lectus nibh.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Different Payment Methods</h4>
+                <div className="answer">
+                    <p>
+                        Proin eget tortor risus. Vestibulum ac diam sit amet quam vehicula
+                        elementum sed sit amet dui. Sed porttitor lectus nibh.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What products do you offer?</h4>
+                <div className="answer">
+                    <p>
+                        Proin eget tortor risus. Vestibulum ac diam sit amet quam vehicula
+                        elementum sed sit amet dui. Sed porttitor lectus nibh.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection;
